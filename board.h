d13308e0cdd285d982268acb98e41380398f9ab7
Amber Lee <amber_Lee1993@HOtmail.com>
2015-03-26 16:50:57 -0400

    Update board.h

--- board.h
+++ board.h
@@ -27,13 +27,13 @@ public:
     void checkL2();
     void checkL3();
     void checkL4()
+    
     void updateBoard(); // recursive function that is called after a swap is made to fill in the board and check for matches
     void hint(); //prints a square or return a move
     void mutateBoard(int r, int c, int c, char t, char s);
     void swap(int r, int c, char d);
     void fillBoard(int l);
     Square *getBoard();
-    int getLevel();
     int getScore();
     void setLevel(int l);
     void setScore(int s);
