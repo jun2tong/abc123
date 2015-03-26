e3da0dd34627246751089b92188dc39508317c2e
noonelah <t.junyong@gmail.com>
2015-03-26 16:48:07 -0400

    Update board.h

--- board.h
+++ board.h
@@ -12,11 +12,12 @@ class Board{
     std::string leftover;
 public:
     Board();
-    void init(std::ifstream &f);
-    void init(int seed);
-    bool validMove(); //check if the move is valid
-    void scramble();
-    void swap(int r, int c, char d);
+    void init(std::ifstream &f); // Jun will do it now
+    void init(int seed); // Jun will do it now
+    
+    bool validMove(); //check if the move is valid (do not have it yet)
+    
+    void swap(int r, int c, char d); 
     bool checkMatch(); // checks whether there exists a row of 3 or column of 3 of the same color
     void resolveMatch();
     void check5Row();
@@ -26,18 +27,17 @@ public:
     void checkL1();
     void checkL2();
     void checkL3();
-    void checkL4()
+    void checkL4();
+    
+    void scramble();
     void updateBoard(); // recursive function that is called after a swap is made to fill in the board and check for matches
-    void hint(); //prints a square or return a move
+    void hint(); //prints a square or return a move 
     void mutateBoard(int r, int c, int c, char t, char s);
-    void swap(int r, int c, char d);
-    void fillBoard(int l);
+    
     Square *getBoard();
-    int getLevel();
-    int getScore();
-    void setLevel(int l);
-    void setScore(int s);
-    void setSeed(int seed);
+    int getLevel(); //amber
+    int getScore(); //amber
+    void setScore(int s); //amber
     ~Board();
     friend std::ostream& operator<<(std::ostream &out, const Board &b);
 };
